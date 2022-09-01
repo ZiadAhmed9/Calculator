b021ad26c4b12d53dce8a307b3866f2097b29627
ZiadAhmed9 <zeyadahmed0@gmail.com>
2022-09-01 20:17:39 +0200

    2 digits results and new OPS are now supported

--- Application.c
+++ Application.c
@@ -12,54 +12,142 @@
 #include "LCD.h"
 #include "KEYPAD.h"
 
-uint8 calc(uint8 operation,uint8 op1,uint8 op2);
+void calc(uint8 operation,uint8 op1,uint8 op2);
+void GET_OP1(void);
+void GET_OP2(void);
+//void GET_OPERATION(void);
+void restart(void);
 
+volatile uint8 op1,op2,operation,check;
 
 int main(void)
 {
-
-	volatile uint8 key,last_val,op1,op2,operation;
+	check=0;
 	LCD_init();
 	KeyPad_init();
+	GET_OP1();
 
-	 key = KeyPad_getPressedKey(); /* get the pressed key number */
-	 LCD_display_int(key);   /*Display the pressed key*/
-	 op1=key;
-	 _delay_ms(300);
+	if(check==1)
+	{
+		main();
+	}
 
-	 key =KeyPad_getPressedKey();
-	 LCD_display_int(key);   /*Display the pressed key*/
-	 operation=key;
-	 _delay_ms(300);
+//	GET_OPERATION();
 
-	 key =KeyPad_getPressedKey();
-	 LCD_display_int(key);   /*Display the pressed key*/
-	 op2=key;
-	 _delay_ms(300);
+	GET_OP2();
 
-	 LCD_Clear();
-	 LCD_display_int(calc(operation,op1,op2));
+	if(check==1)
+	{
+		main();
+	}
 
-	 while(1)
-	    {
+	LCD_Clear();
+
+	calc(operation,op1,op2);
 
 
-	    }
+	while(1)
+	{
+
+
+
+	}
 }
 
-uint8 calc(uint8 operation,uint8 op1,uint8 op2)
+void calc(uint8 operation,uint8 op1,uint8 op2)
 {
 	uint8 result;
 	switch(operation)
 	{
 	case '+' :
 		result=op1+op2;
-
-
+		break;
+	case '-' :
+		result=op1-op2;
+		break;
+	case '*' :
+		result=op1*op2;
+		break;
+	case '/' :
+		result=op1/op2;
+		break;
 	}
-	return result;
+	if(result>=0&&result<10)
+	{
+		LCD_display_int(result);
+	}
+	else if(result>=10&&result<100)
+	{
+		LCD_display_int(result/10);
+		LCD_display_int(result%10);
+	}
 
 }
 
+void GET_OP1(void)
+{
+	volatile int key;
+	key = KeyPad_getPressedKey(); /* get the pressed key number */
+	if(key=='+'||key=='-'||key=='/'||key=='='||key=='A'||key=='*')
+	{
+		LCD_Clear();
+		check=1;
+		return;
+	}
+	LCD_display_int(key);   /*Display the pressed key*/
+	op1=key;
+	key = KeyPad_getPressedKey();
+	if(key=='+'||key=='-'||key=='/'||key=='*')   /*if the entered digit is an arthimetic operator*/
+		{
+			LCD_display_int(key);   /*Display the pressed key*/
+			operation=key;
+			return;
+		}
+	else if(key=='A'||key=='=')
+	{
+		check=1;
+		return;
+	}
+	else
+	{
+	op1=(op1*10)+key;
+	key = KeyPad_getPressedKey();
+	if(key=='+'||key=='-'||key=='/'||key=='*')   /*if the entered digit is an arthimetic operator*/
+			{
+				LCD_display_int(key);   /*Display the pressed key*/
+				operation=key;
+				return;
+			}
+	}
+
+
+}
+
+void GET_OP2(void)
+{
+	uint8 key;
+	key =KeyPad_getPressedKey();
+	if(key=='+'||key=='-'||key=='/'||key=='='||key=='A'||key=='*')
+	{
+		restart();
+		return;
+	}
+	LCD_display_int(key);   /*Display the pressed key*/
+	op2=key;
+}
+
+//void GET_OPERATION(void)
+//{
+//	uint8 key;
+//	key =KeyPad_getPressedKey();
+//	LCD_display_int(key);   /*Display the pressed key*/
+//	operation=key;
+//}
+
+void restart(void)
+{
+	LCD_Clear();
+	check=1;
+}
 
 
